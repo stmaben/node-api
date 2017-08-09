15191d233ad97452b87f940f8cdbc89c8b36cbb3
Benov <sbenov@gmail.com>
2017-08-09 17:50:06 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -23,12 +23,41 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        console.log(`Id:[${id}] is not valid!`);
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+          return res.status(404).send();
+        }
+        console.log(JSON.stringify(todo));
+        res.status(200).send({todo});
+        
+    }).catch((e) => {
+        console.log(e);
+        return res.status(400).send();
+    }); 
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
 module.exports = {app};
-
+ 
 // var newTodo = new Todo({text: 'op3'});
 
 // newTodo.save().then((doc) => {
