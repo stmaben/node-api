325e98a901ec4bcad95947087b8857d714c61974
Benov <sbenov@gmail.com>
2017-08-13 18:31:45 +0300

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -18,11 +18,11 @@ beforeEach((done) => {
     Todo.remove({}).then(() => done());
 });
 
-// beforeEach((done) => {
-//     Todo.remove({}).then(() => { 
-//         return Todo.insertMany(todos);
-//     }).then(() => done());
-// });
+beforeEach((done) => {
+    Todo.remove({}).then(() => { 
+        return Todo.insertMany(todos);
+    }).then(() => done());
+});
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -41,7 +41,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1);
+                    expect(todos.length).toBe(2);
                     expect(todos[0].text).toBe(text);
                     done();
                 }).catch((e) => done(e));
@@ -59,33 +59,33 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then ((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     });
 });
 
-// describe('GET /todos', () => {
-//     it('should get all todos', (done) => {
-//         request(app)
-//             .get('/todos')
-//             .expect(200)
-//             .expect((res) => {
-//                 expect(res.body.todos.length).toBe(2);
-//             })
-//             .end(done);
-//     });
-// });
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
 
-// describe('GET /todos/:id', () => {
-//     it('should return todo doc', (done) => {
-//         request(app)
-//             .get(`/todos/${todos[0]._id.toHexString()}`)
-//             .expect(200)
-//             .expect((res) => {
-//                 expect(res.body.todo.text).toBe(todos[0].text);
-//             })
-//             .end(done);
-//     });
-// });
+describe('GET /todos/:id', () => { 
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+});
